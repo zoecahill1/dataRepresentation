026f76c81d1c98668eea1862ce2de749dc2d492c
Zoe <zcahill90@gmail.com>
2019-12-01 18:44:49 +0000

    added week09 lab

---
+++ week09-a-simple-database/deleteRow.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="datarepresentation"
+)
+
+cursor = db.cursor()
+sql="delete from student where id = %s"
+values = (1,)
+
+cursor.execute(sql, values)
+
+db.commit()
+print("delete done")
